b6e4e11a2a91c65559994120b4816e777e574a4f
Ankit <ankitshaw42@protonmail.com>
2020-09-03 20:00:19 +0530

    complete css class manipulation task

--- 03.CSS_class_manipulation/src/cssClassManipulation.js
+++ 03.CSS_class_manipulation/src/cssClassManipulation.js
@@ -1,5 +1,28 @@
-var cssClass = (function() {
+var cssClass = (function () {
   'use strict';
 
   // code goes here
+
+  function add(element, className) {
+    element.classList.add(className);
+  }
+
+  function remove(element, className) {
+    element.classList.remove(className);
+  }
+
+  function toggle(element, className) {
+    element.classList.toggle(className);
+  }
+
+  function has(element, className) {
+    return element.classList.contains(className);
+  }
+
+  return {
+    add: add,
+    remove: remove,
+    toggle: toggle,
+    has: has,
+  };
 })();
