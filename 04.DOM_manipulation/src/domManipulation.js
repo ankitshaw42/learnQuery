c3c85a38cb8195b14bb7413c90da88e573e2fe0d
Ankit <ankitshaw42@protonmail.com>
2020-09-11 21:09:32 +0530

    complete dom manipulation tasks

--- 04.DOM_manipulation/src/domManipulation.js
+++ 04.DOM_manipulation/src/domManipulation.js
@@ -1,3 +1,48 @@
-var dom = function(){
+var dom = (function () {
   // code goes here
-};
+
+  function remove(target) {
+    if (target && target.remove) {
+      target.remove();
+    }
+  }
+
+  function append(parent, child) {
+    if (parent && parent.append) {
+      parent.append(child);
+    }
+  }
+
+  function prepend(parent, child) {
+    if (parent && parent.prepend) {
+      parent.prepend(child);
+    }
+  }
+
+  function after(element, sibling) {
+    if (element && element.after) {
+      element.after(sibling);
+    }
+  }
+
+  function before(element, sibling) {
+    if (element && element.before) {
+      element.before(sibling);
+    }
+  }
+
+  function val(element) {
+    if (element) {
+      return element.value;
+    }
+  }
+
+  return {
+    remove: remove,
+    append: append,
+    prepend: prepend,
+    after: after,
+    before: before,
+    val: val,
+  };
+})();
